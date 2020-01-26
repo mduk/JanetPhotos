3869e3d768995c3d95e119c7f61e8515e19dc202
Daniel Kendell <daniel.kendell@gmail.com>
2020-01-26 21:35:53 +0000

    static things

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const Years = [
+  2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020
+];
+
+export const Months = [
+  'January', 'February', 'March', 'April', 'May', 'June', 'July',
+  'August', 'September', 'October', 'November', 'December'
+];
+
+export const MonthOrdinals = {
+  January: 1,
+  February: 2,
+  March: 3,
+  April: 4,
+  May: 5,
+  June: 6,
+  July: 7,
+  August: 8,
+  September: 9,
+  October: 10,
+  November: 11,
+  December: 12
+};
+
+export const Tags = [
+  { text: 'Lai Lai', slug: 'lai-lai' },
+  { text: 'Dougie', slug: 'dougie' },
+  { text: 'Jazzmin', slug: 'jazzmin' },
+  { text: 'Twinkle', slug: 'twinkle' },
+  { text: 'Thomas', slug: 'thomas' },
+  { text: 'Monty', slug: 'monty' }
+];
